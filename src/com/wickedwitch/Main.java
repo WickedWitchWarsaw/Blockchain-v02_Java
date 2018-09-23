90fcbc8fe235eaec1d9d1abf61a3f9542df98d00
WickedWitchWarsaw <zuzka.pacak@gmail.com>
2018-09-23 13:35:05 +0200

    SHA256 function

---
+++ src/com/wickedwitch/Main.java
@@ -0,0 +1,12 @@
+package com.wickedwitch;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(SHA256Helper.hash("Hello World!!"));
+        System.out.println(SHA256Helper.hash("Hello World!"));
+
+    }
+
+
+}
---
+++ src/com/wickedwitch/SHA256Helper.java
@@ -0,0 +1,34 @@
+package com.wickedwitch;
+
+import java.security.MessageDigest;
+
+public class SHA256Helper {
+
+    public static String hash(String toHash){
+
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(toHash.getBytes("UTF-8"));
+
+            //END result: hexadecimals not bytes
+            StringBuffer hexadecimalStringBuffer = new StringBuffer();
+
+            for(int i = 0; i < hash.length; i++){
+                String hexadecimal = Integer.toHexString(0xff & hash[i]);
+                if (hexadecimal.length() == 1) {
+                    hexadecimalStringBuffer.append('0');
+                }
+                    hexadecimalStringBuffer.append(hexadecimal);
+
+            }
+
+
+            return hexadecimalStringBuffer.toString();
+
+        } catch (Exception ex){
+            throw new RuntimeException(ex);
+        }
+
+    }
+
+}
